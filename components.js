52668497183d6d4cbf7ffb1d88683a2ac4629bde
9mhmmd99 <9mhmmd99@gmail.com>
2025-08-24 19:28:30 +0300

    Update components.js

--- components.js
+++ components.js
@@ -42,8 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 text-decoration: none;
             }
 .img{ 
-width:50px;
-  height:50px;
+width:40px;
+  height:40px;
 }
             .nav-menu {
                 display: flex;
